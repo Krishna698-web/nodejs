6afae648f9119b10479b7cf91ea15f4c6246b28a
Krishan <kmkrider1999@gmail.com>
2023-06-28 23:46:26 +0530

    Added routers

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
+
+// body parser to parse the incoming data.. {extended: false} as the extension was deprecated add it extensively
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Middleware: functions that has access to all the req, res and next functionalities.
 // next(); //helps to jump to the next middleware
 
-app.use("/", (req, res, next) => {
-  console.log("I'm from / route");
-  next();
-});
+app.use(adminRoutes);
 
-app.use("/users", (req, res, next) => {
-  console.log("I'm from /user route");
-  res.send("I'm from the users route");
-});
+app.use(shopRoutes);
 
 app.listen(3000, () => {
   console.log("Hello");
