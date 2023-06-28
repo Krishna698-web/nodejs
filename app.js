fa506952343780ef313a6273d476b1d0da3f1581
Krishan <kmkrider1999@gmail.com>
2023-06-28 10:06:55 +0530

    Attached middleware

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
-const routing = require("./routes");
 
-const server = http.createServer(routing);
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('I"m in the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('I"m in next middleware');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000, () => {
   console.log("listening");
